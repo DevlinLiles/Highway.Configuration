ba04f70dc14c5bd093c78955f2d790f423086df7
Devlin Liles <devlin@devlinliles.com>
2013-01-29 11:40:25 -0600

    Adding Service backbone, need input from Tim

---
+++ src/Highway.Configuration.Core/Interfaces/IConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ServiceModel;
+using Highway.Configuration.Core.Entities;
+
+namespace Highway.Configuration.Core.Interfaces
+{
+    [ServiceContract]
+    public interface IConfiguration
+    {
+        [OperationContract]
+        Application GetApplication(Guid applicationId);
+
+        [OperationContract]
+        void UpdateSetting(Guid applicationId, ApplicationSetting update);
+
+        [OperationContract]
+        Application AddApplication(Application application);
+
+        [OperationContract]
+        ApplicationSetting AddSetting(Guid applicationId, ApplicationSetting setting);
+    }
+}
---
+++ src/Highway.Configuration.Infrastructure/Services/Configuration.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Highway.Configuration.Core.Entities;
+using Highway.Configuration.Core.Interfaces;
+
+namespace Highway.Configuration.Infrastructure.Services
+{
+    public class Configuration : IConfiguration
+    {
+        public Application GetApplication(Guid applicationId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateSetting(Guid applicationId, ApplicationSetting update)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Application AddApplication(Application application)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ApplicationSetting AddSetting(Guid applicationId, ApplicationSetting setting)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
